bd3b8b19f3060d6fe8098fd17feedb533fc1d2a6
Emma Huang <emma.huang@prezzee.com>
2022-08-22 22:09:06 +1000

    format on save

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -9,15 +9,17 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup({
 	debug = false,
 	sources = {
+		-- formatting.eslint,
 		formatting.prettier.with({
 			extra_filetypes = { "toml" },
 			-- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
 		}),
-		formatting.eslint,
 		-- formatting.black.with { extra_args = { "--fast" }},
 		formatting.yapf.with({
 			extra_args = { "--style={based_on_style: google, column_limit: 100, indent_width: 4}" },
@@ -29,9 +31,22 @@ null_ls.setup({
 		formatting.shfmt,
 		diagnostics.flake8.with({ extra_args = { "--max-line-length=100" } }),
 		diagnostics.eslint,
-    code_actions.eslint,
-    code_actions.eslint_d,
-    code_actions.shellcheck,
-    code_actions.refactoring,
+		code_actions.eslint,
+		code_actions.eslint_d,
+		code_actions.shellcheck,
+		code_actions.refactoring,
 	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.formatting_sync()
+				end,
+			})
+		end
+	end,
 })
